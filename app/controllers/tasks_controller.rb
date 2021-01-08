ba7fbc6f2d28b9861a190b296630f0fe3ac9f8dd
Lena Blank <mine.blank@gmail.com>
2021-01-08 16:49:59 +0100

    index page done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+
+  def index # all tasks
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # read all the tasks
+  get '/tasks', to: 'tasks#index'
+  # create a task
+  get '/tasks/new', to: 'tasks#new' # display the form
+  post '/tasks', to: 'tasks#create'
+  # read one task
+  get '/tasks/:id', to: 'tasks#show'
+  # update a task
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  # delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
