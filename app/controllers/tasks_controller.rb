be374866666f6740cbb016136121978401b12792
Lena Blank <mine.blank@gmail.com>
2021-01-09 15:18:43 +0100

    create and update actions done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,9 +5,13 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
   end
 
   def show
@@ -15,12 +19,22 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # read all the tasks
   get '/tasks', to: 'tasks#index'
-  # create a task
-  get '/tasks/new', to: 'tasks#new' # display the form
-  post '/tasks', to: 'tasks#create'
   # read one task
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/new', to: 'tasks#new' # display the form
+  # create a task
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
   # update a task
   get '/tasks/:id/edit', to: 'tasks#edit'
   patch '/tasks/:id', to: 'tasks#update'
